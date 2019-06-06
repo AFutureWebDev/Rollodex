f993dcfdba1474b6ee4f75964d0d3d27598dea94
AFutureWebDev <jenna.gullick2@gmail.com>
2019-06-05 22:14:03 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+function UserCard (props) {
+  let emailInfo = (
+  <div>
+    <span>{props.user.email}</span>
+  </div>
   );
+
+  let buttonText;
+
+  if (hide) {
+    buttonText = 'Show Details'
+  } else {
+    buttonText = 'Hide Details'
+  }
+
+  return (
+  <div>
+    <img className="pic" src={props.user.picture.thumbnail}/>
+    <div>
+      <div>
+        <span>{props.user.name.first}</span>
+        {' '}
+        <span>{props.user.name.last}</span>
+      </div>
+      {props.hide === false ? emailInfo: emailInfo}
+    </div>
+    <button onClick = {props.onClick}>Show Details</button>
+  </div>
+  )}
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      results: [],
+      isHidden: true
+    }
+  }
+  componentDidMount() {
+    fetch("https://randomuser.me/api?results=25")
+    .then((response) => {
+      return response.json();
+    })
+    .then((json) => {
+      this.setState({
+        results: json.results
+      })
+    })
+  }
+
+  onClick = (event) => {
+    this.setState({
+      isHidden: false
+    })
+  };
+
+    render() {
+    return (
+      <div className="App">
+        {this.state.results.map((user, index) => 
+        <UserCard 
+          key = {index} 
+          user={user} 
+          onClick = {this.onClick} 
+          hide ={this.state.isHidden}/>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
